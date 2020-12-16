41effef36e2a8425935644825adf1d542ad9fd09
Slava Bezgachev <bezgachev@gmail.com>
2020-12-16 21:29:32 +0600

    Here we go

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="../../public/favicon.ico" />
+      </Head>
+
+      <main >
+        <p>Bonk</p>
+      </main>
+
+      <footer >
+        Footer
+      </footer>
+    </div>
+  )
+}
